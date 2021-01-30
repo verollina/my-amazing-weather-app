f8c7d5fbe67d4b9b68943647eea0c5c6abe940b8
verollina <veronica.alessandrini@gmail.com>
2021-01-30 18:02:55 +0000

    Moved html, css and JS into repository

---
+++ src/script.js
@@ -0,0 +1,77 @@
+function formatDate(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let hour = date.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  let minute = date.getMinutes();
+  if (minute < 10) {
+    minute = `0${minute}`;
+  }
+
+  return `${day} ${hour}:${minute}`;
+}
+
+function showTemperature(response) {
+  document.querySelector("h1").innerHTML = response.data.name;
+  document.querySelector("#current-temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#current-temp-description").innerHTML =
+    response.data.weather[0].description;
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind-speed").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+}
+
+function search(city) {
+  let apiKey = "9a16a097a91abca0bb210b3d484b70fe";
+  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+  let units = "metric";
+  let apiUrl = `${apiEndpoint}?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function handleSearch(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-input-form").value;
+  search(city);
+}
+
+function handlePosition(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let apiKey = "9a16a097a91abca0bb210b3d484b70fe";
+  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+  let units = "metric";
+  let apiUrl = `${apiEndpoint}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function handleClick(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(handlePosition);
+}
+
+let now = new Date();
+let returnDate = document.querySelector("#current-date");
+returnDate.innerHTML = formatDate(now);
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSearch);
+
+let button = document.querySelector("#user-location");
+button.addEventListener("click", handleClick);
+
+search("Rome");
