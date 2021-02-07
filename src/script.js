88ae4645cee0f6d549f4af70b6322e416b5b6899
verollina <veronica.alessandrini@gmail.com>
2021-02-07 18:29:20 +0000

    Added forecast for next 3 hours, improved JS for current date/hour and fixed CSS for forecast

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
-function formatDate(date) {
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
   let days = [
     "Sunday",
     "Monday",
@@ -9,6 +10,12 @@ function formatDate(date) {
     "Saturday",
   ];
   let day = days[date.getDay()];
+
+  return `${day} ${formatHours(timestamp)}`;
+}
+
+function formatHours(timestamp) {
+  let date = new Date(timestamp);
   let hour = date.getHours();
   if (hour < 10) {
     hour = `0${hour}`;
@@ -18,12 +25,15 @@ function formatDate(date) {
     minute = `0${minute}`;
   }
 
-  return `${day} ${hour}:${minute}`;
+  return `${hour}:${minute}`;
 }
 
 function showTemperature(response) {
   celsiusTemperature = response.data.main.temp;
   document.querySelector("h1").innerHTML = response.data.name;
+  document.querySelector("#current-date").innerHTML = formatDate(
+    response.data.dt * 1000
+  );
   document.querySelector("#current-temp").innerHTML = Math.round(
     celsiusTemperature
   );
@@ -41,12 +51,43 @@ function showTemperature(response) {
     ) = iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
+function showForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
+  let forecast = null;
+
+  for (let index = 0; index < 6; index++) {
+    forecast = response.data.list[index];
+    forecastElement.innerHTML += `
+    <div class="col-2">
+          <h2>
+          ${formatHours(forecast.dt * 1000)}
+          </h2>
+          <img
+        src="http://openweathermap.org/img/wn/${
+          forecast.weather[0].icon
+        }@2x.png"
+      />
+          <div class="weather-forecast-temperature">
+            <strong>
+            ${Math.round(forecast.main.temp_max)}°
+            </strong> | 
+             ${Math.round(forecast.main.temp_min)}°
+          </div>
+        </div>
+    `;
+  }
+}
+
 function search(city) {
   let apiKey = "9a16a097a91abca0bb210b3d484b70fe";
   let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
   let units = "metric";
   let apiUrl = `${apiEndpoint}?q=${city}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(showTemperature);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showForecast);
 }
 
 function handleSearch(event) {
@@ -64,6 +105,9 @@ function handlePosition(position) {
   let apiUrl = `${apiEndpoint}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
 
   axios.get(apiUrl).then(showTemperature);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(showForecast);
 }
 
 function handleClick(event) {
@@ -96,14 +140,10 @@ fahrenheit.addEventListener("click", convertFahr);
 let celsius = document.querySelector("#celsius-link");
 celsius.addEventListener("click", convertCelsius);
 
-let now = new Date();
-let returnDate = document.querySelector("#current-date");
-returnDate.innerHTML = formatDate(now);
-
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSearch);
 
 let button = document.querySelector("#user-location");
 button.addEventListener("click", handleClick);
 
-search("Rome");
+search("Lisbon");
